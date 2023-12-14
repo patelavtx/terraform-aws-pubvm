04d378c7f747826d392b6bed7cba1b6a316b27f3
patelavtx <apatel@aviatrix.com>
2023-12-14 17:39:04 +0000

    add data block

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+data "aws_ami" "ubuntu_20_04_lts" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+  owners = ["self"]
+}
+
 
 data "http" "ip" {
   url = "https://ifconfig.me"
