3bd067706b5d891d8dec8823fbb3c7ee61288210
patelavtx <apatel@aviatrix.com>
2023-12-14 17:41:17 +0000

    add data block

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ data "aws_ami" "ubuntu_20_04_lts" {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
-  owners = ["self"]
+  owners = ["099720109477"]
 }
 
 
